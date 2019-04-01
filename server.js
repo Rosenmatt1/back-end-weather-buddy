f54d94866f65867a70f2d85b68c57ac587b8f797
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-03-31 21:25:50 -0600

    Added pg and knex

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+const env = process.env.NODE_ENV || 'development'
+const config = require('./knexfile.js')[env]
+const knex = require('knex')(config)
+
+const cors = require('cors')
+const parser = require('body-parser')
+const dotenv = require("dotenv")
+
+app.use(parser.json())
+app.use(cors())
