0819baddc37ab1a037b52e419fcfe433fe048e2b
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-04-04 15:30:08 -0600

    added lat and long to user table migration and seeds

--- seeds/1users.js
+++ seeds/1users.js
@@ -9,18 +9,24 @@ exports.seed = function (knex, Promise) {
           phone: "111-111-1111",
           email: "Matty85@aol.com",
           hashed_password: "4*55bacTge23",
+          lat: 39,
+          long: 105
         },
         {
           name: "Jane",
           phone: "222-222-2222",
           email: "Jane52@aol.com",
           hashed_password: "501cat&459",
+          lat: 51,
+          long: 0
         },
         {
           name: "Henry",
           phone: "333-333-3333",
           email: "Henry13@aol.com",
           hashed_password: "t!he9z%862djf",
+          lat: 42,
+          long: 71
         }
       ])
     })
