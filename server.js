c5ee66234b5a522dccc67d8da69cfe91299c9b0a
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-04-12 16:15:57 -0600

    Added time variable and made the maxTemp and minTemp functions

--- server.js
+++ server.js
@@ -51,6 +51,30 @@ app.post('/create/', (req, res, next) => {
     });
 })
 
+checkMax = () => {
+  const date = new Date()
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  if (alert[0].type === 'max' && alert[0].weatherTemp > alert[0].chosenTemp) {
+    return client.messages.create({
+      to: '+16177193300',
+      from: '+18572693922',
+      body: req.body.message
+    })
+  }
+}
+
+checkMin = () => {
+  if (alert[0].type === 'min' && alert[0].weatherTemp < alert[0].chosenTemp) {
+    return client.messages.create({
+      to: '+16177193300',
+      from: '+18572693922',
+      body: req.body.message
+    })
+  }
+}
+
+
 //for capstone doign the text in a post request,but will use a chron function in the future https://www.npmjs.com/package/node-schedule  a function that runs every night and maps over all alerts and sends text if meets criteria"
 
 app.post('/alert', (req, res, next) => {
@@ -61,122 +85,110 @@ app.post('/alert', (req, res, next) => {
       return alert
     })
     .then(alert => {
-      console.log("Alerter2", alert)
       return knex('users')
         .where('users.id', req.body.user_id)
         .then(user => {
-          console.log("Alerter3", alert)
-          if (alert[0].type === 'max' && alert[0].weatherTemp > alert[0].chosenTemp) {
-            setTimeout(() => {
-              return client.messages.create({
-                to: '+16177193300',
-                from: '+18572693922',
-                body: req.body.message
-              })
-            }, 1000)
-              .then((message) => console.log(message))
-          } else if (alert[0].type === 'min' && alert[0].weatherTemp < alert[0].chosenTemp) {
-             setTimeout(() => {
-               return client.messages.create({
-                to: '+16177193300',
-                from: '+18572693922',
-                body: req.body.message
-              })
-            }, 1000)
-              .then((message) => console.log(message))
-          } else {
-            console.log("it didn't work")
-          }
-        })
-    })
-    .catch((err) => {
-      next(err);
-    });
-})
+          setTimeOut(() => {
+            checkMax()
+          }, 1000)
 
-// 86400000
+          setTimeout(() => {
+
+          })
+        })
+        .catch((err) => {
+          next(err);
+        });
+    })
+
+  // 86400000
+
+  setTimeout(function () {
+    triggerThis();
+    setInterval(triggerThis, 24 * 60 * 60 * 1000);
+  }, firstTriggerAfterMs);
 
   // `+1${user.phone}`
 
-// Date.now()
-// const d = new Date()
-// const n = d.getHours()
+  // Date.now()
+  // const d = new Date()
+  // const n = d.getHours()
 
-// app.get('/', (req, res) => {
-//   return knex('chores')
-//     .then(chores => {
-//       const getHouseDuty = chores.map(chore => {
-//         return knex('roommates')
-//           .join('chores', 'roommates.id', 'chores.roommate_id')
-//           .where('roommates.id', chore.roommate_id)
-//           .select('roommates.name', 'chores.chore')
-//       })
-//       return Promise.all(getHouseDuty).then(result => res.send(result))
-//     })
-// })
-
-
-app.patch('/alert/:id', (req, res, next) => {
-  knex('alerts').update(req.body).where('id', req.params.id).returning('*')
-    .then((alert) => {
-      res.status(200).send(alert);
-    })
-    .catch((err) => {
-      next(err);
-    });
-})
-
-// app.patch('/reminders/:id', (req, res, next) => {
-//   knex('reminders')
-//     .where({ 'id': req.params.id })
-//     .update({ name: "Sweep" })
-//     .returning('*')
-//     .then((rows) => {
-//       res.send(rows);
-//     })
-//     .catch((err) => {
-//       next(err);
-//     });
-// })
-
-app.delete('/alert/:id', (req, res, next) => {
-  knex('alerts').del().where('id', req.params.id).returning('*')
-    .then((alert) => {
-      res.status(200).send("Delete Successful");
-    })
-    .catch((err) => {
-      next(err);
-    });
-})
+  // app.get('/', (req, res) => {
+  //   return knex('chores')
+  //     .then(chores => {
+  //       const getHouseDuty = chores.map(chore => {
+  //         return knex('roommates')
+  //           .join('chores', 'roommates.id', 'chores.roommate_id')
+  //           .where('roommates.id', chore.roommate_id)
+  //           .select('roommates.name', 'chores.chore')
+  //       })
+  //       return Promise.all(getHouseDuty).then(result => res.send(result))
+  //     })
+  // })
 
 
-// app.get('/:tag', (req, res, next) => {
-//   const tag = req.params.tag
-//   if (data.tags.includes(tag)) {
-//     const matching = data.songs.filter(song => song.tags.includes(tag))
-//     res.status(200).send(matching)
-//   } else {
-//     res.status(404).send('No match to your tag')
-//   }
-// })
+  app.patch('/alert/:id', (req, res, next) => {
+    knex('alerts').update(req.body).where('id', req.params.id).returning('*')
+      .then((alert) => {
+        res.status(200).send(alert);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  })
 
-// app.get("/jokes/:tag", (req, res, next) => {
-//   const tag = req.params.tag
-//   const matching = data.jokes.filter(joke => joke.categories.includes(tag))
-//   data.tags.includes(tag)
-//     ? res.status(200).send(matching)
-//     : res.status(404).send("Told you not to mess with the Chuck")
-// })
+  // app.patch('/reminders/:id', (req, res, next) => {
+  //   knex('reminders')
+  //     .where({ 'id': req.params.id })
+  //     .update({ name: "Sweep" })
+  //     .returning('*')
+  //     .then((rows) => {
+  //       res.send(rows);
+  //     })
+  //     .catch((err) => {
+  //       next(err);
+  //     });
+  // })
+
+  app.delete('/alert/:id', (req, res, next) => {
+    knex('alerts').del().where('id', req.params.id).returning('*')
+      .then((alert) => {
+        res.status(200).send("Delete Successful");
+      })
+      .catch((err) => {
+        next(err);
+      });
+  })
+
+
+  // app.get('/:tag', (req, res, next) => {
+  //   const tag = req.params.tag
+  //   if (data.tags.includes(tag)) {
+  //     const matching = data.songs.filter(song => song.tags.includes(tag))
+  //     res.status(200).send(matching)
+  //   } else {
+  //     res.status(404).send('No match to your tag')
+  //   }
+  // })
+
+  // app.get("/jokes/:tag", (req, res, next) => {
+  //   const tag = req.params.tag
+  //   const matching = data.jokes.filter(joke => joke.categories.includes(tag))
+  //   data.tags.includes(tag)
+  //     ? res.status(200).send(matching)
+  //     : res.status(404).send("Told you not to mess with the Chuck")
+  // })
 
 
 
 
-app.use(function (req, res, next) {
-  res.status(404).send("That doesnot exist!")
-})
+  app.use(function (req, res, next) {
+    res.status(404).send("That doesnot exist!")
+  })
 
-app.use(function (req, res, next) {
-  res.status(500).send("Something on our end is broken, please try back!")
-})
+  app.use(function (req, res, next) {
+    res.status(500).send("Something on our end is broken, please try back!")
+  })
 
-app.listen(port, () => console.log(`Weather Alerts on ${port}!`))
+  app.listen(port, () => console.log(`Weather Alerts on ${port}!`))
